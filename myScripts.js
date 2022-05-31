e062f64afa3ea002604efe94a08539b247ebd715
michael o'donovan <mikeodonovan3@gmail.com>
2022-05-31 17:14:32 +0100

    feat: prevent default on submit

--- myScripts.js
+++ myScripts.js
@@ -1,4 +1,17 @@
 
+document.getElementById("submitButton").addEventListener("click", function(event) {
+  event.preventDefault();
+})
+
+const firstNameValue = document.getElementById("firstName").value
+const secondName = document.getElementById("secondName")
+const addressFirstLine = document.getElementById("addressFirstLine")
+const addressSecondLine = document.getElementById("addressSecondLine")
+const postcode = document.getElementById("postcode")
+const country = document.getElementById("country")
+
+// const firstNameValue = firstName.value
+
 function myFunction() {
-  console.log("hello myFunction")
+  console.log("hello dog")
 }
